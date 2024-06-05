745cafec02899d1e1b8565ef6867a9cbfe59bbf0
eliot-christon <eliot.christon@gmail.com>
2024-06-05 09:57:55 +0200

    plot monophonic

--- src/MyMidiObject.py
+++ src/MyMidiObject.py
@@ -9,26 +9,43 @@ class MyMidiObject(mido.MidiFile):
     """Custom class to handle midi files.
     New methods to plot the midi roll and extract only the notes"""
 
-    def __init__(self, path):
+    def __init__(self, path:str, max_channels:int=16):
         """Constructor"""
         mido.MidiFile.__init__(self, path)
+        self.max_channels = max_channels
+        self.types = {"notes": ["note_on", "note_off"],
+                      "changes": ["program_change", "control_change"]}
         self.__divide_tracks()
+        self.__divide_channels()
     
     def __divide_tracks(self):
         """Divide the tracks in 3 categories: meta data, notes and changes"""
         
-        self.meta_data_tracks = [[] for _ in range(len(self.tracks))]
-        self.note_tracks      = [[] for _ in range(len(self.tracks))]
-        self.change_tracks    = [[] for _ in range(len(self.tracks))]
+        self.tracks_meta_data = [[] for _ in range(len(self.tracks))]
+        self.tracks_notes     = [[] for _ in range(len(self.tracks))]
+        self.tracks_changes   = [[] for _ in range(len(self.tracks))]
 
         for i, track in enumerate(self.tracks):
             for msg in track:
-                if msg.type in ["note_on", "note_off"]:
-                    self.note_tracks[i].append(msg)
-                elif msg.type in ["program_change", "control_change"]:
-                    self.change_tracks[i].append(msg)
+                if msg.type in self.types["notes"]:
+                    self.tracks_notes[i].append(msg)
+                elif msg.type in self.types["changes"]:
+                    self.tracks_changes[i].append(msg)
                 else:
-                    self.meta_data_tracks[i].append(msg)
+                    self.tracks_meta_data[i].append(msg)
+    
+    def __divide_channels(self):
+        """Divide all the info by channel"""
+
+        self.channels_events = [[] for _ in range(self.max_channels)]
+
+        for track in self.tracks:
+            for msg in track:
+                if "channel" in msg.dict() and msg.channel < self.max_channels:
+                    channel = msg.channel
+                    self.channels_events[channel].append(msg)
+                else:
+                    continue
 
     def __str__(self) -> str:
         return super().__str__()
@@ -42,6 +59,9 @@ if __name__ == "__main__":
     print("Midi file loaded")
     for i, track in enumerate(midi_file.tracks):
         print('\nTrack {}: {}'.format(i, track.name))
-        print("  notes     :", len(midi_file.note_tracks[i]))
-        print("  changes   :", len(midi_file.change_tracks[i]))
-        print("  meta data :", len(midi_file.meta_data_tracks[i]))
+        print("  notes     :", len(midi_file.tracks_notes[i]))
+        print("  changes   :", len(midi_file.tracks_changes[i]))
+        print("  meta data :", len(midi_file.tracks_meta_data[i]))
+
+    print(type(midi_file.tracks_notes[-2][0]))
+    print(midi_file.tracks_notes[-2][0])
--- src/roll.py
+++ src/roll.py
@@ -2,10 +2,79 @@ __author__ = "Eliot Christon"
 __email__  = "eliot.christon@gmail.com"
 __github__ = "eliot-christon"
 
+from typing import List
+import numpy as np
+import mido
+
 class Roll:
-    """Class reprensenting a midi roll for one track"""
+    """Class reprensenting a midi roll for one track.
+    A midi roll is a matrix where the rows are the notes and the columns are the time steps."""
 
     def __init__(self):
         """Constructor"""
-        pass
+        self.notes = np.array([])
+
+    @classmethod
+    def from_notes(cls, channel_events: List[mido.messages.Message]):
+        """Create a midi roll from a list of notes"""
+        roll = cls()
+        note_min = 1e6
+        note_max = 0
+        time_total = 0
+        for event in channel_events:
+            if "note" in event.dict():
+                note_min = min(note_min, event.note)
+                note_max = max(note_max, event.note)
+            if "time" in event.dict():
+                time_total += event.time
+
+        roll.notes = np.zeros((note_max - note_min + 1, time_total))
+
+        time = 0
+        for event in channel_events:
+            if event.type == "note_on":
+                roll.notes[event.note - note_min, time:time + event.time] += 0
+            elif event.type == "note_off":
+                roll.notes[event.note - note_min, time:time + event.time] -= 1
+            time += event.time
+            
+        return roll
+
+    
+    def __str__(self) -> str:
+        return "Midi Roll: {} notes".format(len(self.notes))
+
+    def __repr__(self) -> str:
+        return "Midi Roll: {} notes".format(len(self.notes))
+
+
+if __name__ == "__main__":
+    # create a midi roll from a list of notes
+
+    def roll_1():
+        notes = [mido.Message('note_on', note=60, time=0),
+                mido.Message('note_off', note=60, time=100),
+                mido.Message('note_on', note=62, time=100),
+                mido.Message('note_off', note=62, time=100),
+                mido.Message('note_on', note=64, time=200),
+                mido.Message('note_off', note=64, time=100),
+                mido.Message('note_on', note=65, time=300),
+                mido.Message('note_off', note=65, time=100)]
         
+        return Roll.from_notes(notes)
+
+    def roll_2(channel_number=4):
+        from MyMidiObject import MyMidiObject
+        midi_file = MyMidiObject('src/AUD_NK0155.mid')
+        return Roll.from_notes(midi_file.channels_events[channel_number])
+
+    roll = roll_2(4)
+
+
+    import matplotlib.pyplot as plt
+
+    plt.matshow(roll.notes, aspect='auto', cmap='Grays')
+    # invert y axis
+    plt.gca().invert_yaxis()
+    plt.show()
+
