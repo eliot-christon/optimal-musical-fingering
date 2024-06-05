da486db8dcb93bd6877b8af8c3b9b836d8c03ede
eliot-christon <eliot.christon@gmail.com>
2024-06-05 09:58:20 +0200

    created MusicSheet object, unsure about for now

---
+++ src/MusicSheet.py
@@ -0,0 +1,8 @@
+__author__ = "Eliot Christon"
+__email__  = "eliot.christon@gmail.com"
+__github__ = "eliot-christon"
+
+from typing import List
+
+class MusicSheet:
+    """Class representing a music sheet (only one track)"""
