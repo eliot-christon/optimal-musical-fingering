aa7ab8a122bbadeb52721c6baed3376ddeb1d213
Eliot CHRISTON <112823641+eliot-christon@users.noreply.github.com>
2024-10-03 10:32:30 +0000

    testing and updating the rule

--- src/rules/getPosFromNotes.py
+++ src/rules/getPosFromNotes.py
@@ -5,8 +5,9 @@ __github__ = "eliot-christon"
 from typing import List
 
 from ..model.instrument.INeck import INeck
+from ..model.Position import NPosition
 
-def getPosFromNotes(notes: List[str], instrument: INeck) -> int:
+def getPosFromNotes(notes: List[str], instrument: INeck) -> NPosition:
     """
     This function takes a list of notes and an instrument and returns a position.
 
@@ -15,10 +16,11 @@ def getPosFromNotes(notes: List[str], instrument: INeck) -> int:
         instrument (INeck): A neck instrument.
 
     Returns:
-        int: The position to play the notes on the instrument.
+        NPosition: The position to play the notes on the instrument.
     """
     
     posiblePositions = instrument.possible_positions(notes)
+    posiblePositions = [pos for pos in posiblePositions if instrument.is_valid_position(pos)]
 
     if len(posiblePositions) == 0:
         return -1
@@ -30,3 +32,15 @@ def getPosFromNotes(notes: List[str], instrument: INeck) -> int:
             bestPosition = position
     
     return bestPosition
+
+
+
+if __name__ == "__main__":
+
+    from ..model.instrument.INeck import Guitar
+    from ..model.utils.note2num import note2num
+
+    instrument = Guitar()
+    notesInt = [note2num(note) for note in ['A2', 'F#3', 'C4', 'E4']]
+
+    print(getPosFromNotes(notesInt, instrument))
