e9459fdb9712236823a8c761a807493c1a2403f8
eliot-christon <eliot.christon.1@etu.sorbonne-universite.fr>
2025-06-19 21:46:46 +0200

    draw neck lines is better

--- src/ui/main.js
+++ src/ui/main.js
@@ -12,13 +12,13 @@ const instrumentSelect = document.getElementById('instrument');
 const neckContainer = document.getElementById('neckContainer');
 
 // Function to create the neck based on selected instrument
-function drawNeck(stringsCount) {
+function drawNeck(stringsCount, fretsCount) {
     neckContainer.innerHTML = ''; // Clear existing neck
 
     // Create a container for the fretboard
     const fretboard = document.createElement('div');
     fretboard.classList.add('fretboard');
-    fretboard.style.gridTemplateColumns = `repeat(${stringsCount}, 1fr)`;
+    fretboard.style.gridTemplateRows = `repeat(${stringsCount}, 1fr)`;
 
     // Generate strings dynamically based on the number of strings
     for (let i = 0; i < stringsCount; i++) {
@@ -34,16 +34,16 @@ function drawNeck(stringsCount) {
 instrumentSelect.addEventListener('change', function() {
     const selectedInstrument = instrumentSelect.value;
     if (selectedInstrument === 'Guitar') {
-        drawNeck(6); // Guitar with 6 strings
+        drawNeck(6, 12); // Guitar with 6 strings
     } else if (selectedInstrument === 'Bass') {
-        drawNeck(4); // Bass with 4 strings
+        drawNeck(4, 12); // Bass with 4 strings
     } else if (selectedInstrument === 'Ukulele') {
-        drawNeck(4); // Ukulele with 4 strings
+        drawNeck(4, 10); // Ukulele with 4 strings
     }
 });
 
 // Draw the default neck on page load
-drawNeck(6); // Guitar is default
+drawNeck(6, 12); // Guitar is default
 
 // Handle submit button click
 // API call : post at url: "http://localhost:8000/getPosFromNotes"
@@ -51,7 +51,7 @@ const apiUrl = "http://localhost:8000/getPosFromNotes";
 document.getElementById('submit').addEventListener('click', async function() {
     const notes = document.getElementById('notes').value;
     const selectedInstrument = instrumentSelect.value;
-    alert(`Notes: ${notes}, Instrument: ${selectedInstrument}`);
+    // alert(`Notes: ${notes}, Instrument: ${selectedInstrument}`);
 
     // create notes array from input
     const notesArray = notes.split(' ').map(note => note.trim()).filter(note => note !== '');
@@ -69,11 +69,12 @@ document.getElementById('submit').addEventListener('click', async function() {
         });
 
         if (!response.ok) {
-            throw new Error('Network response was not ok, details: ' + response.statusText);
+            throw new Error('Network response was not ok, status: ' + response.statusText);
         }
 
         const data = await response.json();
-        console.log(data); // Handle the response data as needed
+        console.log(data);
+
     } catch (error) {
         console.error('Error:', error);
     }
